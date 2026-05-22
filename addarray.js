3a8578429529179c6815d2bf2fef527e17a9e3e4
Eswarchenna <chennaeshwar7@gmail.com>
2026-05-22 19:22:37 +0530

    Add files via upload

---
+++ addarray.js
@@ -0,0 +1,9 @@
+var flowers = [];
+
+function onClickAddToArray() {
+    debugger;
+    var inputName = document.getElementById("txtInputName").value;
+    flowers.push(inputName);
+    document.getElementById("pResult").innerHTML = flowers;
+    document.getElementById("txtInputName").value = "";
+}
